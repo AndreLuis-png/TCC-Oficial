482fdc7cd34b6af3f8d246eb9468036856fc7bcd
luanbielap07 <luan.biel.a.p@gmail.com>
2026-09-25 14:32:19 -0300

    alteração na definição do banco (talvez de uma k hda)

--- app.py
+++ app.py
@@ -4,12 +4,10 @@ from flask_mysqldb import MySQL
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'chave_secreta_almoxarifado'
 
-# Configurações do MySQL
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '' 
-app.config['MYSQL_DB'] = 'almoxarifado_db'
-
+def conectar_banco():
+return mysql.connector.connect(
+host="db",
+user="root",
 mysql = MySQL(app)
 
 # Importação e registro das Blueprints
